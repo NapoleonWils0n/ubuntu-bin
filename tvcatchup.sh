0d76639a6a1fc9386c00b177d556cff858bbd7b3
Daniel J Wilcox <danieljwilcox@gmail.com>
2018-12-02 15:33:24 +0000

    removed script

--- tvcatchup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-input="$1"
-wscrape=$(whereis wscrape | awk '{print $2}')
-#echo $($wscrape $input)
-/usr/bin/ffmpeg -loglevel fatal -user-agent Kodi/14.1 -i $($wscrape $input) -codec copy -bsf:v h264_mp4toannexb,dump_extra -tune zerolatency -f mpegts pipe:1
